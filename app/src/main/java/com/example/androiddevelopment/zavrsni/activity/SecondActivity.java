ff5f2601c55a8aca493739ec7f9624679bb9337d
mikans86 <zarmiroslav@gmail.com>
2017-03-20 19:48:03 +0100

    Zavrsen drugi activity bez dodavanja broja

--- app/src/main/java/com/example/androiddevelopment/zavrsni/activity/SecondActivity.java
+++ app/src/main/java/com/example/androiddevelopment/zavrsni/activity/SecondActivity.java
@@ -1,11 +1,28 @@
 package com.example.androiddevelopment.zavrsni.activity;
 
+import android.app.Dialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.androiddevelopment.zavrsni.R;
 import com.example.androiddevelopment.zavrsni.db.DataHelper;
@@ -16,10 +33,19 @@ import com.j256.ormlite.android.apptools.OpenHelperManager;
 import java.sql.SQLException;
 import java.util.List;
 
+import static com.example.androiddevelopment.zavrsni.activity.FirstActivity.NOTIF_STATUS;
+import static com.example.androiddevelopment.zavrsni.activity.FirstActivity.NOTIF_TOAST;
+
 public class SecondActivity extends AppCompatActivity {
 
     private DataHelper databaseHelper;
     private Kontakt k;
+    private SharedPreferences prefs;
+
+    private EditText ime;
+    private EditText prezime;
+    private EditText adresa;
+    private String slika;
 
 
 
@@ -33,7 +59,79 @@ public class SecondActivity extends AppCompatActivity {
         if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int key = getIntent().getExtras().getInt(FirstActivity.ACTOR_KEY);
+
+        try {
+            k = getDatabaseHelper().getKontaktDao().queryForId(key);
+
+            ime = (EditText) findViewById(R.id.Ime2);
+            prezime = (EditText) findViewById(R.id.Prezime2);
+            adresa = (EditText) findViewById(R.id.Adresa2);
+
+
+            ime.setText(k.getmIme());
+            prezime.setText(k.getmPrezime());
+            adresa.setText(k.getmAdresa());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        final ListView listView = (ListView) findViewById(R.id.brojevi);
+
+        try {
+            List<Brojevi> list = getDatabaseHelper().getBrojeviDao().queryBuilder()
+                    .where()
+                    .eq(Brojevi.FIELD_NAME_USER, k.getmId())
+                    .query();
+
+            ListAdapter adapter = new ArrayAdapter<>(this, R.layout.list_item, list);
+            listView.setAdapter(adapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Brojevi b = (Brojevi) listView.getItemAtPosition(position);
+                    Toast.makeText(SecondActivity.this, b.getBBroj() + " " + b.getBTip() , Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
     }
+
+
+    private void showStatusMesage(String message){
+        NotificationManager mNotificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.ic_launcher);
+        mBuilder.setContentTitle("Pripremni test");
+        mBuilder.setContentText(message);
+
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_add);
+
+        mBuilder.setLargeIcon(bm);
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+
+    private void showMessage(String message){
+        boolean toast = prefs.getBoolean(NOTIF_TOAST, false);
+        boolean status = prefs.getBoolean(NOTIF_STATUS, false);
+
+        if (toast){
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+
+        if (status){
+            showStatusMesage(message);
+        }
+    }
+
+
 
     private void refresh() {
         ListView listview = (ListView) findViewById(R.id.brojevi);
@@ -58,6 +156,112 @@ public class SecondActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+
+    private void addInitCateogry(){
+        try {
+            if (getDatabaseHelper().getBrojeviDao().queryForAll().size() == 0){
+                Brojevi kucni = new Brojevi();
+                kucni.setBTip("Kucni");
+
+                Brojevi mobilni = new Brojevi();
+                mobilni.setBTip("mobilni");
+
+                getDatabaseHelper().getBrojeviDao().create(kucni);
+                getDatabaseHelper().getBrojeviDao().create(mobilni);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.dodaj_broj:
+                //OTVORI SE DIALOG UNESETE INFORMACIJE
+                final Dialog dialog = new Dialog(this);
+                dialog.setTitle("Dodavanje broja telefona");
+                dialog.setContentView(R.layout.broj_dijalog);
+
+                final Spinner productsSpinner = (Spinner) dialog.findViewById(R.id.vrsta);
+                List<Brojevi> list = null;
+                try {
+                    list = getDatabaseHelper().getBrojeviDao().queryForAll();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                ArrayAdapter<Brojevi> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+                productsSpinner.setAdapter(dataAdapter);
+                productsSpinner.setSelection(0);
+
+                Button add = (Button) dialog.findViewById(R.id.dodaj_broj);
+                add.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText broj = (EditText) dialog.findViewById(R.id.broj2);
+                        Brojevi tip = (Brojevi) productsSpinner.getSelectedItem();
+
+
+                        Brojevi b = new Brojevi();
+                        b.setBBroj(broj.getText().toString());
+                       // b.setBrojevi(tip);
+
+
+                        try {
+                            getDatabaseHelper().getBrojeviDao().create(b);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        //URADITI REFRESH
+                        refresh();
+
+                        showMessage("New movie added to actor");
+
+                        dialog.dismiss();
+                    }
+                });
+                final Button cancel = (Button) dialog.findViewById(R.id.cancel);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+
+                break;
+            case R.id.edit:
+                //POKUPITE INFORMACIJE SA EDIT POLJA
+                k.setmIme(ime.getText().toString());
+                k.setmPrezime(prezime.getText().toString());
+                k.setmAdresa(adresa.getText().toString());
+
+                try {
+                    getDatabaseHelper().getKontaktDao().update(k);
+
+                    showMessage("Dodati podaci o kontaktu");
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            case R.id.remove:
+                try {
+                    getDatabaseHelper().getKontaktDao().delete(k);
+
+                    showMessage("Kontakt izbrisan");
+
+                    finish(); //moramo pozvati da bi se vratili na prethodnu aktivnost
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
