a20265431ad738af67ebd08996abd6b203920421
mikans86 <zarmiroslav@gmail.com>
2017-03-20 18:30:28 +0100

    Uradjen ToolBar

--- app/src/main/java/com/example/androiddevelopment/zavrsni/activity/FirstActivity.java
+++ app/src/main/java/com/example/androiddevelopment/zavrsni/activity/FirstActivity.java
@@ -1,15 +1,113 @@
 package com.example.androiddevelopment.zavrsni.activity;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.androiddevelopment.zavrsni.R;
+import com.example.androiddevelopment.zavrsni.db.DataHelper;
+import com.example.androiddevelopment.zavrsni.db.model.Kontakt;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private DataHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.first_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void showStatusMesage(String message){
+        NotificationManager mNotificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.ic_launcher);
+        mBuilder.setContentTitle("Zavrsni");
+        mBuilder.setContentText(message);
+
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_add);
+
+        mBuilder.setLargeIcon(bm);
+
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        refresh();
+    }
+
+    private void refresh() {
+        ListView listview = (ListView) findViewById(R.id.kontakti_list);
+
+        if (listview != null){
+            ArrayAdapter<Kontakt> adapter = (ArrayAdapter<Kontakt>) listview.getAdapter();
+
+            if(adapter!= null)
+            {
+                try {
+                    adapter.clear();
+                    List<Kontakt> list = getDatabaseHelper().getKontaktDao().queryForAll();
+
+                    adapter.addAll(list);
+
+                    adapter.notifyDataSetChanged();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public DataHelper getDatabaseHelper() {
+        if (databaseHelper == null) {
+            databaseHelper = OpenHelperManager.getHelper(this, DataHelper.class);
+        }
+        return databaseHelper;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // nakon rada sa bazo podataka potrebno je obavezno
+        //osloboditi resurse!
+        if (databaseHelper != null) {
+            OpenHelperManager.releaseHelper();
+            databaseHelper = null;
+        }
     }
 }
